6d3e54b2cd9c46d900c2557d040f23ebb67fb4e1
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2026-04-29 11:23:52 +0530

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import SignupComponent from "./UserComponents/SignupComponent"
+export const BaseUrl='https://esphere-blog-server-1.onrender.com'
 
 function App() {
  
--- src/UserComponents/SignupComponent.jsx
+++ src/UserComponents/SignupComponent.jsx
@@ -1,9 +1,45 @@
 
-import React from "react";
-
+import React, { useState } from "react";
+import { BaseUrl } from "../App";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+console.log(BaseUrl)
 const SignupComponent=()=>{
+    // const navigate=useNavigate()
+
+    const[form,setForm]=useState({
+        username:"",
+        email:"",
+        password:"",
+        role:"author"
+    })
+
+    const changeHandler=(e)=>{
+        setForm({...form,[e.target.username]:[e.target.value]})
+    }
+
+    const handleSubmit=async()=>{
+        try{
+            const res=await axios.post(`${BaseUrl}/apiUser/SignupUser`,form)
+            alert(res.data.message)
+            console.log(res.data.message)
+        }
+        catch(err){
+            console.log(err)
+            alert("signup failed")
+        }
+    }
     return(
-        <h1>Hello</h1>
+        <div>
+        <input type="text" placeholder="enter username" onChange={changeHandler}>Username</input>
+        <input type="email" placeholder="enter email" onChange={changeHandler}>Email</input>
+        <input type="text" placeholder="enter password" onChange={changeHandler}>Password</input>
+        <select name="role" onChange={changeHandler}>Select User
+            <option value='author'>Author</option>
+            <option value='admin'>Admin</option>
+        </select>
+        <button onClick={handleSubmit}>Signup</button>
+        </div>
     )
 }
  export default SignupComponent
